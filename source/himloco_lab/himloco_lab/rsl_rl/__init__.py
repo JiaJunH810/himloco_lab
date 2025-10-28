6fd69ea5c3ea8e1fe610f3029f6f7c78e64bb510
Isaac <1812924685@qq.com>
2025-10-28 14:33:59 +0800

    migrate rsl-rl into himloco_lab forder

--- source/himloco_lab/himloco_lab/rsl_rl/__init__.py
+++ source/himloco_lab/himloco_lab/rsl_rl/__init__.py
@@ -27,3 +27,20 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+"""
+HimLoco RSL-RL implementation with history-informed models.
+"""
+
+# Export HIM implementations
+from .algorithms.him_ppo import *
+from .modules.him_actor_critic import *
+from .modules.him_estimator import *
+from .storage.him_rollout_storage import *
+from .runners.him_on_policy_runner import *
+
+# # Export original implementations for reference
+# from .algorithms.ppo import *
+# from .modules.actor_critic import *
+# from .storage.rollout_storage import *
+# from .runners.on_policy_runner import *
